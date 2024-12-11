2c849d7460757ca51090ad0283ad237bbd56beda
Robert <indiepulsecorp@gmail.com>
2024-12-10 23:50:42 -0600

    Set up

---
+++ TimeBank/TimeBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeBankApp.swift
+//  TimeBank
+//
+//  Created by Robert Greenwood on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TimeBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
